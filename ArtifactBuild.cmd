5a797653b489cad43d53b486857f22f97f2eeac4
Jason Zhang <zjjc123@hotmail.com>
2021-02-16 18:24:19 -0800

    Adding Path

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Release" -ArchiveFileName "SampleX86.zip" -Format Zip
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
+powershell Compress-7Zip "Release" -ArchiveFileName "SampleX86.zip" -Format Zip -Path ./
+powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -Path ./
 :exit
 popd
 @echo on
