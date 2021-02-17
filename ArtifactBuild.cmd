e03206848ee200dc12ea77ac530d8a8dfb29f873
Jason Zhang <zjjc123@hotmail.com>
2021-02-16 18:49:00 -0800

    Fix Formatting

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,5 @@
 @echo off
-pushd "%~dp0"
-powershell Compress-7Zip "mine-sweeper\bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip -Path ./
-powershell Compress-7Zip "mine-sweeper\bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -Path ./
+powershell Compress-7Zip -Path ".\mine-sweeper\bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip -OutputPath "./"
+powershell Compress-7Zip -Path ".\mine-sweeper\bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -OutputPath "./"
 :exit
-popd
 @echo on
