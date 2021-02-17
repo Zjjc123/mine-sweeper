64e53dc7008fded1a5f1cb5d5933a9ef0a31c0d3
Jason Zhang <zjjc123@hotmail.com>
2021-02-16 18:54:41 -0800

    CMD Name Change

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,5 +1,5 @@
 @echo off
-powershell Compress-7Zip -Path ".\mine-sweeper\bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip -OutputPath "./"
-powershell Compress-7Zip -Path ".\mine-sweeper\bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -OutputPath "./"
+powershell Compress-7Zip -Path ".\mine-sweeper\bin\x86\Release" -ArchiveFileName "mine-sweeperx86.zip" -Format Zip -OutputPath "./"
+powershell Compress-7Zip -Path ".\mine-sweeper\bin\x64\Release" -ArchiveFileName "mine-sweeperx64.zip" -Format Zip -OutputPath "./"
 :exit
 @echo on
