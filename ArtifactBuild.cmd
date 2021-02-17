581ed1491c13acc437f90c6b6e65cbdbee729c1c
Jason Zhang <zjjc123@hotmail.com>
2021-02-16 18:27:27 -0800

    Updating File Path

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Release" -ArchiveFileName "SampleX86.zip" -Format Zip -Path ./
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -Path ./
+powershell Compress-7Zip "mine-sweeper\bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip -Path ./
+powershell Compress-7Zip "mine-sweeper\bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip -Path ./
 :exit
 popd
 @echo on
